ab32e26a5822465c54a55c6f86bd6d4ca7781c3d
Natalia <sh.nat.vlad@gmail.com>
2024-05-17 15:00:04 +0500

    change: add context in class UserFinder

--- src/components/UserFinder.js
+++ src/components/UserFinder.js
@@ -2,14 +2,11 @@ import { Fragment, Component } from 'react';
 
 import Users from './Users';
 import classes from './UserFinder.module.css';
-
-const DUMMY_USERS = [
-  { id: 'u1', name: 'Max' },
-  { id: 'u2', name: 'Manuel' },
-  { id: 'u3', name: 'Julie' },
-];
+import UsersContext from '../store/users-context';
 
 class UserFinder extends Component {
+    static contextType = UsersContext;
+
     constructor() {
         super();
         this.state = {
@@ -20,12 +17,12 @@ class UserFinder extends Component {
 
     componentDidMount() {
         // Send http request...
-        this.setState({ filteredUsers: DUMMY_USERS });
+        this.setState({ filteredUsers: this.context.users });
     }
 
     componentDidUpdate(prevProps, prevState) {
         if (prevState.searchTerm !== this.state.searchTerm) {
-            return this.setState({filteredUsers: DUMMY_USERS.filter((user) => user.name.includes(this.state.searchTerm))});
+            return this.setState({filteredUsers: this.context.users.filter((user) => user.name.includes(this.state.searchTerm))});
         }
     }
 
@@ -36,10 +33,10 @@ class UserFinder extends Component {
     render() {
         return (
             <Fragment>
-              <div className={classes.finder}>
-                <input type='search' onChange={this.searchChangeHandler.bind(this)} />
-              </div>
-              <Users users={this.state.filteredUsers} />
+                <div className={classes.finder}>
+                    <input type='search' onChange={this.searchChangeHandler.bind(this)} />
+                </div>
+                <Users users={this.state.filteredUsers} />
             </Fragment>
           );
     }
